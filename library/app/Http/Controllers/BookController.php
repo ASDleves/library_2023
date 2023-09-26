3708aa75dd76efdb2f91107b5262edaf0c1caf49
ASDleves <ricsi20020302@gmail.com>
2023-09-26 15:50:53 +0200

    blade

--- library/app/Http/Controllers/BookController.php
+++ library/app/Http/Controllers/BookController.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index(){
+        return Book::all();
+    }
+
+    public function show($id){
+        return Book::find($id);
+    }
+    public function destroy($id){
+        Book::find($id)->delete();
+        
+        return redirect('/book/list');
+
+    }
+    public function update(Request $request, $id){
+        $book = Book::find($id);
+        $book->author = $request->author;
+        $book->title = $request->title;
+        $book->pieces = $request->pieces;
+        $book->save();
+        return redirect('/book/list');
+    }
+
+    public function store(Request $request){
+        $book = new Book();
+        $book->author = $request->author;
+        $book->title = $request->title;
+        $book->pieces = $request->pieces;
+        $book->save();
+        return redirect('/book/list');
+    }
+    public function listView(){
+        $books = Book::all();
+        return view("book.list",["books" => $books]);
+    }
+    public function newView(){
+    return view("book.new");
+
+    }
+    public function editView($id){
+        $book = Book::find($id);
+        return view("book.edit", ["book"=> $book]);
+
+    }
 }
--- library/routes/web.php
+++ library/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Models\Book;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api/book', [BookController::class, 'index']);
+Route::get('/api/book/{id}', [BookController::class, 'show']);
+Route::put('/api/book/{id}', [BookController::class, 'update']);
+Route::post('/api/book', [BookController::class, 'store']);
+Route::delete('/api/book/{id}', [BookController::class, 'destroy']);
+
+
+Route::get('/book/list', [BookController::class, 'listview']);
